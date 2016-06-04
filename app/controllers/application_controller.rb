aaa07879c3a8f9f0ef93b9bab87c5bbbdc3b53f8
David Battersby <info@davidbattersby.com>
2016-06-05 01:56:17 +0800

    move header to partial, add upvote and downvote links, style add complaint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_user
+
+  def set_user
+    @user = current_user if user_signed_in?
+  end
 end
