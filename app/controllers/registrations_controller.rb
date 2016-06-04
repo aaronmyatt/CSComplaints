b3dfc519dee3c1e378326f661c4ca9a718d04470
snowflakekiller <karuna.skj@gmail.com>
2016-06-04 19:23:33 +0800

    devise modal saves the user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def create
-    build_resource
-
+    build_resource(sign_up_params)
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
