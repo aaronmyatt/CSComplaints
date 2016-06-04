c1df7616790504debe8e58bc9094a669b255ea61
snowflakekiller <karuna.skj@gmail.com>
2016-06-04 17:07:48 +0800

    flesh out upvote controller

--- app/controllers/upvote_controller.rb
+++ app/controllers/upvote_controller.rb
@@ -1,2 +1,13 @@
 class UpvoteController < ApplicationController
+
+  def upvote(complaint)
+    return unless set_user
+    complaint.liked_by @user 
+  end
+
+  private
+
+  def set_user
+    @user = current_user if user_signed_in?
+  end
 end
