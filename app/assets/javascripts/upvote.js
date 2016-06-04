2e7b3a1cd8d9f53e671b1e7721b880c57c92e279
snowflakekiller <karuna.skj@gmail.com>
2016-06-04 23:44:26 +0800

    upvotes work better

--- app/assets/javascripts/upvote.js
+++ app/assets/javascripts/upvote.js
@@ -1,10 +1,10 @@
 $(function() {
-  $('#upvote_arrow').click(function() {
+  $('.upvote_arrow').click(function() {
     $.ajax({
         type: "POST",
         url:"/upvote",
         data:{
-            id: $('#upvote_arrow').data('complaint-id')
+            id: $(this).data('complaint-id')
         },
         dataType: 'JSON',
         success: function(data, status){
