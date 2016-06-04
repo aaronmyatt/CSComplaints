bf5e2ce3b95c8ea586223ef27f296fd3b67da51f
David Battersby <info@davidbattersby.com>
2016-06-04 13:57:37 +0800

    add fields to complaint model and db

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,2 +1,7 @@
 class Complaint < ActiveRecord::Base
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :lat, presence: true
+  validates :long, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604044403) do
+ActiveRecord::Schema.define(version: 20160604055136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "complaints", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id",     null: false
+    t.string   "title",       null: false
+    t.text     "description", null: false
+    t.string   "lat"
+    t.string   "long"
   end
 
   create_table "users", force: :cascade do |t|
