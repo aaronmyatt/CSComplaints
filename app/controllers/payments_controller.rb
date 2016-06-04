2d355c61a7497edd6821bb44df701e992356113f
Aaron Myatt <aaronmyatt@gmail.com>
2016-06-05 06:33:55 +0800

    payment#create redirects to molpay view checkout

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,12 @@
 class PaymentsController < InheritedResources::Base
+  before_action :set_user
+  before_action :set_complaint
+  before_action :set_payment, only: :create
+  before_action :authenticate_user!
 
-  def send
-
+  def create
+    @payment.save
+    redirect_to MolpayService.from_payment_model(@payment).build
   end
 
   def success
@@ -10,8 +15,21 @@ class PaymentsController < InheritedResources::Base
 
   private
 
-    def payment_params
-      params.require(:payment).permit(:amount, :references, :references, :provider)
-    end
+  def payment_params
+    params.require(:payment).permit(:amount, :user_id, :complaint_id)
+  end
+
+  def set_complaint
+    @complaint = Complaint.find(payment_params[:complaint_id])
+  end
+
+  def set_user
+    @user = User.find(payment_params[:user_id])
+  end
+
+  def set_payment
+    @payment = Payment.new(payment_params)
+  end
+
 end
 
