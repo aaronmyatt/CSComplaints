568df217dd14fefe1fd952448961bc465dd51f41
David Battersby <info@davidbattersby.com>
2016-06-05 00:26:52 +0800

    remove null validation from lat long, may add this back in later

--- db/migrate/20160604161230_remove_null_check_on_lat_lng.rb
+++ db/migrate/20160604161230_remove_null_check_on_lat_lng.rb
@@ -1,5 +1,9 @@
 class RemoveNullCheckOnLatLng < ActiveRecord::Migration
-  def change
+  def self.up
+    change_column :complaints, :latitude, :float, :null => true
+    change_column :complaints, :longitude, :float, :null => true
+  end
+  def self.down
     change_column :complaints, :latitude, :float, :null => false
     change_column :complaints, :longitude, :float, :null => false
   end
