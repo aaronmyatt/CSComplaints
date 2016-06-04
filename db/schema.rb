e8ecbc80b53a6db60edda043ba40487938250f14
snowflakekiller <karuna.skj@gmail.com>
2016-06-04 12:55:15 +0800

    added provider and UID columns for omniauth strategies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604044403) do
+ActiveRecord::Schema.define(version: 20160604045405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20160604044403) do
     t.datetime "updated_at", null: false
     t.string   "email"
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
